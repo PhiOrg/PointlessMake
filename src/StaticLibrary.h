edcd72d02d2324793db12411a0f5fcc749055574
EnachescuAlin <alinenachescu033@gmail.com>
2015-06-08 16:20:56 +0300

    Added new modifications.

--- src/StaticLibrary.h
+++ src/StaticLibrary.h
@@ -2,18 +2,24 @@
 #define _STATICLIBRARY_H__
 
 #include <string>
+#include <vector>
 
 class StaticLibrary
 {
     public:
-        StaticLibrary();
+        StaticLibrary(unsigned long long int);
         virtual~ StaticLibrary();
         void SetName(std::string&);
         void SetCompiler(std::string&);
         void SetCflags(std::string&);
         void SetFiles(std::string&);
+        void CompileFiles();
     private:
-        std::string name, compiler, cflags, files;
+        void RemovePointlessSpaces(std::string&);
+        unsigned long long int GetLastModification(std::string&);
+        unsigned long long int lastCompilation;
+        std::string name, compiler, cflags;
+        std::vector<std::string> files, objectFiles;
 };
 
 #endif
